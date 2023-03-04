907834af36f252c5837167d043ddae4a93b96206
ZVINZV <zain.business.co@gmail.com>
2023-03-04 16:36:59 +0300

    Finshed Work

---
+++ js/main.js
@@ -0,0 +1,200 @@
+let select = document.getElementById("select");
+
+let depNight = document.querySelector(".fajr");
+let shorok = document.querySelector(".sunset");
+let dohour = document.querySelector(".doher");
+let magrib = document.querySelector(".maghrib");
+
+let cityname = document.querySelector(".cityname");
+let date = document.querySelector(".date");
+
+let main_city = [
+  {
+    name: {
+      ar: "النجف الاشرف",
+      en: "Najaf",
+    },
+    date: {
+      day: new Date().toLocaleDateString("ar-iq", { weekday: "long" }),
+      num: new Date().toLocaleDateString("en-us"),
+    },
+    location: {
+      lant: "32.0217018",
+      long: "44.3644243",
+    },
+    time: {},
+  },
+  {
+    name: {
+      ar: "البصرة",
+      en: "Basra",
+    },
+    date: {
+      day: new Date().toLocaleDateString("ar-iq", { weekday: "long" }),
+      num: new Date().toLocaleDateString("en-us"),
+    },
+    location: {
+      lant: "30.5312179",
+      long: "47.8417508",
+    },
+    time: {},
+  },
+  {
+    name: {
+      ar: "بابل",
+      en: "babel",
+    },
+    date: {
+      day: new Date().toLocaleDateString("ar-iq", { weekday: "long" }),
+      num: new Date().toLocaleDateString("en-us"),
+    },
+    location: {
+      lant: "32.4732208",
+      long: "44.4393714",
+    },
+    time: {},
+  },
+  {
+    name: {
+      ar: "ذي قار",
+      en: "theiKar",
+    },
+    date: {
+      day: new Date().toLocaleDateString("ar-iq", { weekday: "long" }),
+      num: new Date().toLocaleDateString("en-us"),
+    },
+    location: {
+      lant: "31.039336",
+      long: "46.3499263",
+    },
+    time: {},
+  },
+  {
+    name: {
+      ar: "كربلاء",
+      en: "karbala",
+    },
+    date: {
+      day: new Date().toLocaleDateString("ar-iq", { weekday: "long" }),
+      num: new Date().toLocaleDateString("en-us"),
+    },
+    location: {
+      lant: "32.60751",
+      long: "44.0464657",
+    },
+    time: {},
+  },
+  {
+    name: {
+      ar: "واسط",
+      en: "kout",
+    },
+    date: {
+      day: new Date().toLocaleDateString("ar-iq", { weekday: "long" }),
+      num: new Date().toLocaleDateString("en-us"),
+    },
+    location: {
+      lant: "32.5129091",
+      long: "45.8451352",
+    },
+    time: {},
+  },
+  {
+    name: {
+      ar: "القادسية",
+      en: "alKadisia",
+    },
+    date: {
+      day: new Date().toLocaleDateString("ar-iq", { weekday: "long" }),
+      num: new Date().toLocaleDateString("en-us"),
+    },
+    location: {
+      lant: "31.973471",
+      long: "44.9014464",
+    },
+    time: {},
+  },
+  {
+    name: {
+      ar: "ميسان",
+      en: "misan",
+    },
+    date: {
+      day: new Date().toLocaleDateString("ar-iq", { weekday: "long" }),
+      num: new Date().toLocaleDateString("en-us"),
+    },
+    location: {
+      lant: "31.8403761",
+      long: "47.1458781",
+    },
+    time: {},
+  },
+  {
+    name: {
+      ar: "المثنى",
+      en: "alMothana",
+    },
+    date: {
+      day: new Date().toLocaleDateString("ar-iq", { weekday: "long" }),
+      num: new Date().toLocaleDateString("en-us"),
+    },
+    location: {
+      lant: "31.3299899",
+      long: "45.2914677",
+    },
+    time: {},
+  },
+];
+
+
+let x = []
+main_city.forEach((element) => {
+  x.push(element.name.ar)
+  axios
+    .get(
+      `https://hq.alkafeel.net/Api/init/init.php?timezone=+3&long=${element.location.long}&lati=${element.location.lant}&v=jsonPrayerTimes`
+    )
+    .then((respons) => {
+      for (let [key, value] of Object.entries(respons.data)) {
+        if (key != "powerdby" && key != "date") {
+          element.time[key] = value;
+        }
+      }
+
+      // 
+    })
+    .catch((reject) => {
+      console.log(error);
+    });
+});
+x.sort()
+for (let i of x) {
+  select.innerHTML += `<option value="${i}">${i}</option>`;
+}
+select.addEventListener("change", function () {
+  main_city.forEach((element) => {
+    if (element.name.ar === this.value) {
+      editNameAndDate(
+        element.name.ar,
+        element.date.day + " - " + element.date.num,
+        element.time.fajir,
+        element.time.sunset,
+        element.time.doher,
+        element.time.maghrib
+      );
+      window.scroll({
+        top: 0,
+        behavior: "smooth"
+      })
+    }
+  });
+});
+
+function editNameAndDate(name, dates, fajir, sunsets, daur, mg) {
+  cityname.innerHTML = name;
+  date.innerHTML = dates;
+  depNight.innerHTML = fajir + "<span class='time'>صباحاً</span>";
+  shorok.innerHTML = sunsets + "<span class='time'>صباحاً</span>";
+  dohour.innerHTML = daur + "<span class='time'>مساءً</span>";
+  magrib.innerHTML = mg + "<span class='time'>مساءً</span>";
+}
